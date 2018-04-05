9c4806af5e230ff400c28ac25ca2a4b8435994bc
Crystal O <crystalodi@gmail.com>
2018-04-05 12:19:14 -0500

    include spotify and twitter modules

--- liri.js
+++ liri.js
@@ -2,3 +2,10 @@ require("dotenv").config()
 var fs = require("fs");
 var request = require("request");
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+console.log(spotify);
+console.log(client);
