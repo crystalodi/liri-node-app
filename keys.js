8043db061a693460ba734766cba0d60bcd6e7c4f
Crystal O <crystalodi@gmail.com>
2018-04-04 22:49:44 -0500

    remove dotenv module

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
 console.log("this is loaded");
-var dotEnv = require('dotenv');
-dotEnv.config();
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
