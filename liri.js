046c5155e53b957857250932fd062b3ff9fac9f9
Crystal O <crystalodi@gmail.com>
2018-04-06 16:37:13 -0500

    parse command arguments

--- liri.js
+++ liri.js
@@ -8,14 +8,24 @@ var spotify = new spotify_module(keys.spotify);
 var client = new twitter_module(keys.twitter);
 console.log(spotify);
 console.log(client)
-var liriCommand = process.argv[2]
+var nodeArguments = process.argv;
+var liriCommand = nodeArguments[2];
+var liriAction = "";
+//put actions in an array and join on " "
+var actionArray = [];
+if(nodeArguments[3]) {
+  for(var i = 3; i < nodeArguments.length; i++) {
+    liriAction = liriAction + " " + nodeArguments[i];
+  }
+  liriAction = liriAction.trim();
+}
 if(liriCommand) {
   liriCommand = liriCommand.toLowerCase();
 } 
 else {
   return console.log("Please provide a command");
 }
-
+console.log(liriAction.length);
 switch(liriCommand) {
   case "my-tweets":
     getMyTweets();
